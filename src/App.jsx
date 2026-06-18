d68acef61a4fe70046d15eef224a6a6cf262a4cf
surya-kapil <suryakapil.bam@gmail.com>
2026-06-18 17:34:40 +0530

    Added Caraousel Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import "./App.css";
 // eslint-disable-next-line import/extensions
-import logo from "./logo.svg";
+import Product from "./Product";
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
+    <Product />
   </div>
 );
 
