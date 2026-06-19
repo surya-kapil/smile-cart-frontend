8b643937340dc49cae09c12d20d59e846e890612
surya-kapil <suryakapil.bam@gmail.com>
2026-06-19 12:14:04 +0530

    Implemented Axios interceptors to set http headers and to transform response

--- src/Product.jsx
+++ src/Product.jsx
@@ -19,7 +19,8 @@ const Product = () => {
     try {
       // await waitNSec();
       const response = await productsApi.show();
-      setProduct(response.data);
+      setProduct(response);
+      console.log(response);
     } catch (error) {
       console.log("Error: ", error);
     } finally {
@@ -39,9 +40,8 @@ const Product = () => {
     );
   }
 
-  const { name, description, mrp, offer_price, image_urls, image_url } =
-    product;
-  const discountPrice = mrp - offer_price;
+  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  const discountPrice = mrp - offerPrice;
   const discountPercentage = ((discountPrice / mrp) * 100).toFixed(1);
 
   return (
@@ -53,17 +53,14 @@ const Product = () => {
         <div className="mt-16 flex gap-4">
           <div className="w-2/5">
             <div className="flex justify-center gap-16">
-              <Carousel
-                imageUrls={append(image_url, image_urls)}
-                title={name}
-              />
+              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
             </div>
           </div>
           <div className="w-3/5 space-y-4">
             <Typography>{description}</Typography>
             <Typography>MRP: {mrp}</Typography>
             <Typography className="font-semibold">
-              Offer price: {offer_price}
+              Offer price: {offerPrice}
             </Typography>
             <Typography className="font-semibold text-green-600">
               {discountPercentage}% off
