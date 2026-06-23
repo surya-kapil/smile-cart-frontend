7e849a240efacb82975ad850128565b7be7186e5
surya-kapil <suryakapil.bam@gmail.com>
2026-06-23 10:57:35 +0530

    Adjusted ProductList component to use query parameter info

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -3,69 +3,92 @@ import { useState } from "react";
 import { Header, PageLoader } from "components/Commons/index";
 import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "components/constants";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
-import useDebouncer from "hooks/useDebounce";
+import useFuncDebounce from "hooks/useFuncDebounce";
+import useQueryParams from "hooks/useQueryParams";
+import { filterNonNull } from "neetocist";
 import { Search } from "neetoicons";
 import { Input, NoData, Pagination } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, mergeLeft } from "ramda";
+import { useHistory } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
-  const [searchKey, setSearchKey] = useState(null);
-  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
-  const debouncedSearchKey = useDebouncer(searchKey);
+  const history = useHistory();
+  const queryParams = useQueryParams();
+  const { page, pageSize, searchTerm = "" } = queryParams;
+  const [searchKey, setSearchKey] = useState(searchTerm);
   const productsParams = {
-    searchTerm: debouncedSearchKey,
-    page: currentPage,
-    pageSize: DEFAULT_PAGE_SIZE,
+    searchTerm,
+    page: Number(page) || DEFAULT_PAGE_INDEX,
+    pageSize: Number(pageSize) || DEFAULT_PAGE_SIZE,
   };
 
   const { data: { products = [], totalProductsCount } = {}, isLoading } =
     useFetchProducts(productsParams);
+
+  const handlePageNavigation = page =>
+    history.replace(
+      buildUrl(
+        routes.products.index,
+        mergeLeft({ page, pageSize: DEFAULT_PAGE_SIZE }, queryParams)
+      )
+    );
+
+  const updateQueryParams = useFuncDebounce(value => {
+    const params = {
+      page: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+      searchTerm: value || null,
+    };
+
+    history.replace(buildUrl(routes.products.index, filterNonNull(params)));
+  });
 
   if (isLoading) {
     return <PageLoader />;
   }
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div className="m-2">
-          <Header
-            shouldShowBackButton={false}
-            title="Smile Cart"
-            actionBlock={
-              <Input
-                placeholder="Search products"
-                prefix={<Search />}
-                type="search"
-                onChange={event => {
-                  setSearchKey(event.target.value);
-                  setCurrentPage(DEFAULT_PAGE_INDEX);
-                }}
-              />
-            }
-          />
-        </div>
-        {isEmpty(products) ? (
-          <NoData className="h-full w-full" title="No products to show" />
-        ) : (
-          <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
-            {products.map(product => (
-              <ProductListItem key={product.slug} {...product} />
-            ))}
-          </div>
-        )}
+    <div className="flex flex-col">
+      <div className="m-2">
+        <Header
+          shouldShowBackButton={false}
+          title="Smile Cart"
+          actionBlock={
+            <Input
+              placeholder="Search products"
+              prefix={<Search />}
+              type="search"
+              value={searchKey}
+              onChange={({ target: { value } }) => {
+                updateQueryParams(value);
+                setSearchKey(value);
+              }}
+            />
+          }
+        />
       </div>
+      {isEmpty(products) ? (
+        <NoData className="h-full w-full" title="No products to show" />
+      ) : (
+        <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
+          {products.map(product => (
+            <ProductListItem key={product.slug} {...product} />
+          ))}
+        </div>
+      )}
       <div className="mb-5 self-end">
         <Pagination
           count={totalProductsCount}
-          navigate={page => setCurrentPage(page)}
-          pageNo={currentPage || DEFAULT_PAGE_INDEX}
-          pageSize={DEFAULT_PAGE_SIZE}
+          navigate={handlePageNavigation}
+          pageNo={Number(page) || DEFAULT_PAGE_INDEX}
+          pageSize={Number(pageSize) || DEFAULT_PAGE_SIZE}
         />
       </div>
-    </>
+    </div>
   );
 };
 
